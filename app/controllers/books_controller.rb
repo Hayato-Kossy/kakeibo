94fccae3419fdaff0c2fdc8bad459074893fb497
Hayato Koshi <kshyt2501@icloud.com>
2023-05-12 04:31:16 +0900

    delete-done

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,9 +18,28 @@ class BooksController < ApplicationController
             render :new
         end
     end
+    def edit 
+        @book = Book.find(params[:id])
+    end
 
-    private 
-        def book_params
-            book_params = params.require(:book).permit(:year,:month,:inout,:category,:amount)
+    def update
+        @book = Book.find(params[:id])
+        if @book.update(book_params)
+          redirect_to @book
+        else
+          render :edit
         end
+    end
+    
+    def destroy
+        @book = Book.find(params[:id])
+        @book.destroy
+        redirect_to books_path         
+    end
+      
+
+    private
+    def book_params
+      params.require(:book).permit(:year, :month, :inout, :category, :amount)
+    end
 end   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root "books#index"
-  resources :books
+  resources :books, except: :destroy
+  delete '/books/:id', to: 'books#destroy', as: 'destroy_book'  
 end
